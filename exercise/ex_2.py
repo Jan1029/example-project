5b015cc1c2f58a59614ff53ef839cc569f7c145c
Jan1029 <jan.galazka2108@gmail.com>
2023-03-27 12:38:14 +0200

    Dodanie trapezu

--- exercise/ex_2.py
+++ exercise/ex_2.py
@@ -11,9 +11,20 @@ print("obwod kwadratu wynosi" + str(obwod) + "a pole" +str(pole) +".")
 #prostokat
 
 b=5
-obwod = 2 * a + 2 * b
-pole = a * b
+obwod_prostokata = 2 * a + 2 * b
+pole_prostokata = a * b
 #Obwod prostokata wynosi 30, a pole 50
+print("Obwod prostokata wynosi" + str(obwod_prostokata)+ "a pole" + str(pole_prostokata) + ".")
 
+#trapez
 
+a = 4
+b = 13
+c = 9
+d = 12
+h = 12
+
+obwod_trapez = a + b + c + d
+pole_trapez = ((a + c) * h) / 2
+print("Obwod trapezu wynosi" + str(obwod_trapez)+ "a pole" + str(pole_trapez) + ".")
 
