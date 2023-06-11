fc7c291edcead22f00d4727424951d1f0d14363a
jan <jan.galazka2108@gmail.com>
2023-06-11 21:37:00 +0200

    Fibonacci

--- solo-work/solo_rekursja.py
+++ solo-work/solo_rekursja.py
@@ -29,5 +29,25 @@ print (silnia (3))
 print (silnia (1))
 print (silnia (-1))
 
+#fibonacci
+# is n<0?
+    # yes->none
+# is n=0?
+    # yes->0
+#is n=1
+    #yes->1
+# result = (n-1) + (n-2)
 
-
+def fibonacci(n):
+    if n<0:
+        return None
+    if(n == 0):
+        return 0
+    if(n == 1):
+        return 1
+    return fibonacci(n-1)+fibonacci(n-2)
+print (fibonacci(0))
+print (fibonacci(1))
+print (fibonacci(5))
+print (fibonacci(10))
+print (fibonacci(-1))
